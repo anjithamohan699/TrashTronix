95c28118f76c989e07587fe058185c0d620d0ad0
Sahadiya <sidhiqsahadiya@gmail.com>
2024-11-04 15:11:07 +0530

    footer

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -3,6 +3,7 @@ export default function AdminLayout({ children }) {
       <div>
         {children}
         {/* Footer */}
+        {/* Footer */}
         <footer className="bg-white text-black text-center p-4 mt-auto">
     &copy; 2024 Trashtronix. All rights reserved.
   </footer>
